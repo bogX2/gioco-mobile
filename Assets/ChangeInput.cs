fc3b405bd4b3245c5a38e122dc88291f4304f07a
maregames81 <marepersonale@gmail.com>
2024-09-20 17:25:22 +0200

    Agginuta pagina di login (prima del package)

---
+++ Assets/ChangeInput.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ChangeInput : MonoBehaviour
+{
+
+    EventSystem system;
+    public Selectable firstInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+        system = EventSystem.current;
+        firstInput.Select();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
