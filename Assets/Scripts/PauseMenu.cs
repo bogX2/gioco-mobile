87b26696ee1b85638596dff78581790f8eb00182
Bogdan Andrei Tutuianu <128369457+bogX2@users.noreply.github.com>
2024-09-20 13:18:39 +0200

    aggiunta setting musica

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -2,16 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.UI;
+using UnityEngine.Audio;
 public class PauseMenu : MonoBehaviour
 {
 
+    public Slider volumeSlider;
+    public AudioMixer mixer;
+    private float value;
     public  static bool Paused=false;
     public GameObject PausedMenuCanvas;
     // Start is called before the first frame update
     void Start()
     {
         Time.timeScale=1f;
+         mixer.GetFloat("MyExposedParam",out value);
+        volumeSlider.value=value;
+       
     }
 
     // Update is called once per frame
@@ -43,5 +50,10 @@ public class PauseMenu : MonoBehaviour
         SceneManager.LoadScene("MainMenu");
     }
 
+
+    public void SetVolume(){
+        mixer.SetFloat("MyExposedParam",volumeSlider.value);
+
+    }
 
 }
